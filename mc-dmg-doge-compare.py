2cdfe8e4a6a0a13d3e76c6a22dcda32cb2293ccd
Compsogbrickus <compsogbrickus@gmail.com>
2020-03-21 16:59:29 -0400

    Excepts optional arguments, added Auth

--- mc-dmg-doge-compare.py
+++ mc-dmg-doge-compare.py
@@ -7,15 +7,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Find the best armor build given a situation.')
 
 #Add command-line arguments
-parser.add_argument('damage', metavar='damage', type=float, nargs='1',
+parser.add_argument('damage', metavar='damage', default=30.0, type=float, nargs='?',
                     help='received damage from an attack')
-parser.add_argument('weapon_damage', metavar='weapon damage', type=float, nargs='1',
+parser.add_argument('weapon_damage', metavar='weapon damage', default=10.0, type=float, nargs='?',
                     help='base damage your held weapon deals')
-parser.add_argument('weapon_speed', metavar='weapon speed', type=float, nargs='1',
+parser.add_argument('weapon_speed', metavar='weapon speed', default=1.6, type=float, nargs='?',
                     help='base attack speed your held weapon has')
-parser.add_argument('bow_damage', metavar='bow damage', type=float, nargs='1',
+parser.add_argument('bow_damage', metavar='bow damage', default=10.0, type=float, nargs='?',
                     help='base damage your held bow deals')
-parser.add_argument('arrow_speed', metavar='arrow speed', type=float, nargs='1',
+parser.add_argument('arrow_speed', metavar='arrow speed', default=0.0, type=float, nargs='?',
                     help='base bonus speed for your bow-fired arrows')
 
 #Compile the arguments
@@ -36,11 +36,12 @@ creds  = ServiceAccountCredentials.from_json_keyfile_name('mc-dmg-doge-reader.js
 client = gspread.authorize(creds)
 
 #Open every worksheet.
-helmets     = client.open('Armor Stats').get_worksheet(1)
-chestplates = client.open('Armor Stats').get_worksheet(2)
-leggings    = client.open('Armor Stats').get_worksheet(3)
-boots       = client.open('Armor Stats').get_worksheet(4)
-offhands    = client.open('Armor Stats').get_worksheet(5)
+stats       = client.open('Armor Stats')
+helmets     = stats.get_worksheet(0)
+chestplates = stats.get_worksheet(1)
+leggings    = stats.get_worksheet(2)
+boots       = stats.get_worksheet(3)
+offhands    = stats.get_worksheet(4)
 
 #Get the key-value pairs of data from every worksheet.
 helmet_data     = helmets.get_all_records()
